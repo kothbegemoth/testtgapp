f7f3251affd6647afceb1244bd018036cdd1385c
kothbegemoth <rinapadb@icloud.com>
2025-06-04 00:55:11 +0500

    fasd

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const tg = window.Telegram.WebApp;
 tg.expand(); // Раскрыть на весь экран
 
-document.getElementById('myButton').addEventListener('click', () => {
-    tg.showAlert(print());
-});
+
+if (window.Telegram && Telegram.WebApp) {
+  const user = Telegram.WebApp.initDataUnsafe.user;
+  
+  if (user) {
+    tg.showAlert("Данные пользователя:", user);
+    
+    // Доступные поля:
+    const userId = user.id; // ID пользователя
+    const firstName = user.first_name; // Имя
+    const lastName = user.last_name; // Фамилия (может отсутствовать)
+    const username = user.username; // Ник (@username)
+    const photoUrl = user.photo_url; // Аватар (URL)
+    
+    // Используем данные в интерфейсе
+    document.getElementById('user-name').textContent = `${firstName} ${lastName || ''}`;
+    document.getElementById('user-avatar').src = photoUrl || 'default-avatar.jpg';
+  }
+}
 
 document.getElementById('profileBtn').addEventListener('click', function() {
             const profile = document.getElementById('userProfile');
@@ -13,11 +29,3 @@ document.getElementById('profileBtn').addEventListener('click', function() {
                 profile.style.display = 'block';
             }
         });
-
-function print() {
-     const user = Telegram.WebApp.initDataUnsafe.user;
-  
-    if (user) {
-        console.log(user)
-    }
-}
