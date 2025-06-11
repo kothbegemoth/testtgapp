184c4975c8063c84ad90897379b724aaa2100b61
kothbegemoth <rinapadb@icloud.com>
2025-06-11 08:12:52 +0500

    vb

--- tasks.js
+++ tasks.js
@@ -14,7 +14,7 @@ document.getElementById('checkAnswer').addEventListener('click', showFeedback)
 
 function showFeedback() {
     if (window.Telegram && Telegram.WebApp) {
-        tg.showAlert('Тут будет проверка ответа на задачку\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nfghnfxjnsrzfjnzjnz!');
+        tg.showAlert('Тут будет проверка ответа на задачку\n\n\n\n\n\n\n\n\n\n\n\n\ут будет проверка ответа на задачку\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nfghnfxjnsrzfjnzjnz!n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nfghnfxjnsrzfjnzjnz!');
         newTask;
     }
     else {
